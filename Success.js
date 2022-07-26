cb8a3d5b217fdc62c6d1019791d834e699623be0
MarwanAliii <84754292+MarwanAliii@users.noreply.github.com>
2022-07-26 18:24:57 -0400

    Create Success.js

---
+++ Success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+      <div className="form-content-right">
+          <div classname="form-success">We've recieved your request</div>
+    </div>
+  )
+}
+
+export default Success
